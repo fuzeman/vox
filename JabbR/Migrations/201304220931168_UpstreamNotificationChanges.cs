a954cf8cee9174d5fafef24bdbde8158834f182f
Dean Gardiner <gardiner91@gmail.com>
2013-04-22 22:31:11 +1200

    Fix for migration issues.

--- JabbR/Migrations/201304220931168_UpstreamNotificationChanges.cs
+++ JabbR/Migrations/201304220931168_UpstreamNotificationChanges.cs
@@ -7,10 +7,19 @@ namespace JabbR.Models.Migrations
     {
         public override void Up()
         {
-            AddColumn("dbo.ChatMessages", "MessageType", c => c.Int(nullable: false));
+            AddColumn("dbo.ChatMessages", "MessageType", c => c.Int(nullable: false, defaultValue: 0));
             AddColumn("dbo.ChatMessages", "ImageUrl", c => c.String());
             AddColumn("dbo.ChatMessages", "Source", c => c.String());
-            AddColumn("dbo.Notifications", "RoomKey", c => c.Int(nullable: false));
+
+            AddColumn("dbo.Notifications", "RoomKey", c => c.Int(nullable: true));
+
+            Sql(@"UPDATE n
+SET n.RoomKey = m.Room_Key
+FROM dbo.Notifications n
+INNER JOIN dbo.ChatMessages m on m.[Key] = n.MessageKey");
+
+            AlterColumn("dbo.Notifications", "RoomKey", c => c.Int(nullable: false));
+
             AddForeignKey("dbo.Notifications", "RoomKey", "dbo.ChatRooms", "Key", cascadeDelete: true);
             CreateIndex("dbo.Notifications", "RoomKey");
         }
