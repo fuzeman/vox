96b94ec5de21f17ef75aa146f635fa585a95a14d
Dean Gardiner <gardiner91@gmail.com>
2013-07-04 13:59:09 +1200

    Better italics using new regex pattern.

--- JabbR/Chat.ui.js
+++ JabbR/Chat.ui.js
@@ -482,8 +482,20 @@
     }
     
     function processItalics(content) {
-        //Replaces /test/ occurrences in message with <i>test</i> so you can use italics
-        return content.replace(/\B\/([^\/]*?)\/\B/g, "<i>$1</i>").replace("<i></i>", "");
+        var re = /(?:\*|_)([^\*_]*)(?:\*|_)/g,
+            match = null,
+            result = content;
+
+        //Replaces *test* occurrences in message with <i>test</i> so you can use italics
+        while ((match = re.exec(result)) != null) {
+            if (match[1].length > 0) {
+                var head = result.substring(0, match.index);
+                var tail = result.substring(match.index + match[0].length, result.length);
+                result = head + "<i>" + match[1] + "</i>" + tail;
+            }
+        }
+
+        return result;
     }
 
     function isFromCollapsibleContentProvider(content) {
