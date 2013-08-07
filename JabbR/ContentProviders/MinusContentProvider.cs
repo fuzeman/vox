31d2c647c21cb9921848005b9c54a7a83d3b81f8
Dean Gardiner <gardiner91@gmail.com>
2013-08-07 20:48:35 +1200

    Added a minus.com content provider to ensure images are displayed in https://

---
+++ JabbR/ContentProviders/MinusContentProvider.cs
@@ -0,0 +1,34 @@
+﻿using JabbR.ContentProviders.Core;
+using Microsoft.Security.Application;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JabbR.ContentProviders
+{
+    public class MinusContentProvider : CollapsibleContentProvider
+    {
+        private static readonly Regex UriRegex = new Regex("^https?://i(\\d+)?\\.minus.com/(?<id>.*?)\\.(?<ext>.*)$", RegexOptions.IgnoreCase);
+
+        protected override Task<ContentProviderResult> GetCollapsibleContent(ContentProviderHttpRequest request, Match match)
+        {
+            if (!match.Success) throw new ArgumentException("Invalid match, expected a successful match", "match");
+
+            var url = String.Format("https://i.minus.com/{0}.{1}", match.Groups["id"], match.Groups["ext"].Value);
+
+            return TaskAsyncHelper.FromResult(new ContentProviderResult
+            {
+                Content = String.Format(ImageContentProvider.HtmlFormat,
+                    Encoder.HtmlAttributeEncode(request.RequestUri.AbsoluteUri),
+                    Encoder.HtmlAttributeEncode(url)),
+                Title = url,
+                Weight = 1
+            });
+        }
+
+        public override Match Match(Uri uri)
+        {
+            return UriRegex.Match(uri.AbsoluteUri);
+        }
+    }
+}
