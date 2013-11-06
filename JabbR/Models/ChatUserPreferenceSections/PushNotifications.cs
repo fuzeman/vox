1fa9ea7260b6e672e39f8d3942ebb5e74295a0dd
Dean Gardiner <gardiner91@gmail.com>
2013-11-06 23:51:27 +1300

    Added PushNotifications section to ChatUserPreferences and [jabbr/components/client-settings]

---
+++ JabbR/Models/ChatUserPreferenceSections/PushNotifications.cs
@@ -0,0 +1,25 @@
+﻿namespace JabbR.Models.ChatUserPreferenceSections
+{
+    public class PushNotifications
+    {
+        public NotifyMyAndroid NMA { get; set; }
+
+        public Pushover Pushover { get; set; }
+    }
+
+    public class NotifyMyAndroid
+    {
+        public bool Enabled { get; set; }
+
+        public string APIKey { get; set; }
+    }
+
+    public class Pushover
+    {
+        public bool Enabled { get; set; }
+
+        public string UserKey { get; set; }
+
+        public string DeviceName { get; set; }
+    }
+}
