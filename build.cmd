ace53300e745ae2b73b4c6ecaa41233a88cca719
Dean Gardiner <gardiner91@gmail.com>
2014-01-09 14:25:25 +1300

    Added +x to .nuget\NuGet.exe, Updated build.cmd to call VsDevCmd.bat

--- build.cmd
+++ build.cmd
@@ -4,5 +4,6 @@ if "%config%" == "" (
    set config=debug
 )
 
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat"
 .nuget\NuGet.exe restore JabbR.sln -configFile %~dp0\.nuget\NuGet.config -nocache
 msbuild %~dp0\Build\Build.proj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
