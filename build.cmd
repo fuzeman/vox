fe0fbc2a15d581652e1447cda9bfd9944b7a4bf6
Dean Gardiner <gardiner91@gmail.com>
2014-01-09 14:48:49 +1300

    Updated build.cmd to properly setup environment

--- build.cmd
+++ build.cmd
@@ -1,9 +1,26 @@
-@echo Off
+@echo off
+
 set config=%1
 if "%config%" == "" (
    set config=debug
 )
 
-"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\VsDevCmd.bat"
-.nuget\NuGet.exe restore JabbR.sln -configFile %~dp0\.nuget\NuGet.config -nocache
-msbuild %~dp0\Build\Build.proj /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+@if "%FrameworkDir32%"=="" goto error
+@if "%FrameworkVersion32%"=="" goto error
+@if "%Framework35Version%"=="" goto error
+
+@set FrameworkDir=%FrameworkDir32%
+@set FrameworkVersion=%FrameworkVersion32%
+@set PATH=%FrameworkDir%%Framework35Version%;%PATH%
+@set PATH=%FrameworkDir%%FrameworkVersion%;%PATH%
+
+.nuget\NuGet.exe restore "%~dp0JabbR.sln" -configFile "%~dp0.nuget\NuGet.config" -nocache
+msbuild "%~dp0Build\Build.proj" /p:Configuration="%config%" /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+@goto end
+
+:error
+echo ERROR: Missing environment variable
+@goto end
+
+:end
