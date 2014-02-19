523a355b2c517e4d2d8c4f24d5a695d142b7d988
Dean Gardiner <gardiner91@gmail.com>
2014-02-20 04:34:09 +1300

    reddit content provider now adds query parameters (context) to location

--- Vox/ContentProviders/RedditContentProvider.cs
+++ Vox/ContentProviders/RedditContentProvider.cs
@@ -8,7 +8,7 @@ namespace JabbR.ContentProviders
 {
     public class RedditContentProvider : CollapsibleContentProvider
     {
-        public readonly Regex UriRegex = new Regex(@"https?:\/\/(?<value>(\w+.)?reddit.com(\/r\/\w+)?(\/comments\/\w+\/\w+(\/\w+)?)?)(\/)?", RegexOptions.IgnoreCase);
+        public readonly Regex UriRegex = new Regex(@"^https?:\/\/(?<value>(\w+.)?reddit.com(\/r\/\w+)?(\/comments\/\w+\/\w+(\/\w+)?)?)(\/)?(?<parameters>\?.*?)?$", RegexOptions.IgnoreCase);
 
         public RedditContentProvider()
         {
@@ -20,6 +20,9 @@ namespace JabbR.ContentProviders
         protected override Task<ContentProviderResult> GetCollapsibleContent(ContentProviderHttpRequest request, Match match)
         {
             var value = match.Groups["value"].Value;
+
+            if (match.Groups["parameters"].Success)
+                value += match.Groups["parameters"].Value;
 
             return TaskAsyncHelper.FromResult(new ContentProviderResult
             {
